1ab01f469003cbb10cfa96d30a066a5b5658ccbc
96rla <96rlarms@gmail.com>
2020-11-09 13:37:56 +0900

    //카테고리 액티비티

--- app/src/main/java/com/example/qna/CategorySettingActivity.java
+++ app/src/main/java/com/example/qna/CategorySettingActivity.java
@@ -3,6 +3,8 @@ package com.example.qna;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 
 public class CategorySettingActivity extends AppCompatActivity {
@@ -12,16 +14,22 @@ public class CategorySettingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_setting);
 
-        final CheckBox cb1 = (CheckBox)findViewById(R.id.chk_hobby);
-        final CheckBox cb2 = (CheckBox)findViewById(R.id.chk_);
-        final CheckBox cb3 = (CheckBox)findViewById(R.id.checkBox3);
-        final CheckBox cb4 = (CheckBox)findViewById(R.id.checkBox4);
+        final CheckBox cb1 = (CheckBox)findViewById(R.id.chk_love);
+        final CheckBox cb2 = (CheckBox)findViewById(R.id.chk_hobby);
+        final CheckBox cb3 = (CheckBox)findViewById(R.id.chk_relationship);
+        final CheckBox cb4 = (CheckBox)findViewById(R.id.chk_sports);
+        final CheckBox cb5 = (CheckBox)findViewById(R.id.chk_movie);
+        final CheckBox cb6 = (CheckBox)findViewById(R.id.chk_music);
 
 
-        Button b = (Button)findViewById(R.id.button1);
-        final TextView tv = (TextView)findViewById(R.id.textView2);
+        Button b = (Button)findViewById(R.id.btn_submit);
 
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-        출처: https://bitsoul.tistory.com/45 [Happy Programmer~]
+            }
+        });
+
     }
 }
