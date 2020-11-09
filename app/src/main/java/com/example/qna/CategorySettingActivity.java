c71519d9d511d8b1f61a2bf2971a1749c8c9c723
96rla <96rlarms@gmail.com>
2020-11-09 13:31:15 +0900

    //카테고리 xml

--- app/src/main/java/com/example/qna/CategorySettingActivity.java
+++ app/src/main/java/com/example/qna/CategorySettingActivity.java
@@ -3,6 +3,7 @@ package com.example.qna;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
 
 public class CategorySettingActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class CategorySettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_setting);
+
+        final CheckBox cb1 = (CheckBox)findViewById(R.id.chk_hobby);
+        final CheckBox cb2 = (CheckBox)findViewById(R.id.chk_);
+        final CheckBox cb3 = (CheckBox)findViewById(R.id.checkBox3);
+        final CheckBox cb4 = (CheckBox)findViewById(R.id.checkBox4);
+
+
+        Button b = (Button)findViewById(R.id.button1);
+        final TextView tv = (TextView)findViewById(R.id.textView2);
+
+
+        출처: https://bitsoul.tistory.com/45 [Happy Programmer~]
     }
 }
