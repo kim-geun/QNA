4d1bc944e7858bbd2c3ec49614f32041a4e9f5da
eonju <deer8203@naver.com>
2020-11-09 10:57:19 +0900

    11/09 Login/Signup/Main

--- app/src/main/java/com/example/qna/MainActivity.java
+++ app/src/main/java/com/example/qna/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.qna;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        // Temporary code - not to fix manifest
+        Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
 }
