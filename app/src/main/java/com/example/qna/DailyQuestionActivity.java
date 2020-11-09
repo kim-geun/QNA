7e9efed7da25106313827baab7203ebb25f3d14e
seongjinah <5829341@naver.com>
2020-11-09 13:06:53 +0900

    1. DailyQuestion xml 완성 2. QuestionResult xml 대충 완성

--- app/src/main/java/com/example/qna/DailyQuestionActivity.java
+++ app/src/main/java/com/example/qna/DailyQuestionActivity.java
@@ -2,13 +2,36 @@ package com.example.qna;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DailyQuestionActivity extends AppCompatActivity {
 
+    Button toIndex, submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_question);
+
+        toIndex = findViewById(R.id.daily_question_toindex);
+        submit  = findViewById(R.id.daily_question_submit);
+
+        toIndex.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DailyQuestionActivity.this, QuestionIndexActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DailyQuestionActivity.this, QuestionResultActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
