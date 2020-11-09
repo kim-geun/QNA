cad8e0b7bfe5be3f0bbc15b6a42a79e328d8593e
seongjinah <5829341@naver.com>
2020-11-09 09:22:22 +0900

    1. Activity 만들기 Intro, Login, DailyQuestion, QuestionIndex, QuestionResult, Setting, CategorySetting

---
+++ app/src/main/java/com/example/qna/CategorySettingActivity.java
@@ -0,0 +1,14 @@
+package com.example.qna;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CategorySettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_setting);
+    }
+}
