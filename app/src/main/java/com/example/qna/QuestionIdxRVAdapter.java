e9fe4fa974d8c79f9026732bbc9604544a481922
Gyuwon1231 <kwkim1231@naver.com>
2020-11-09 13:32:56 +0900

    userdata Questiondata 구성

---
+++ app/src/main/java/com/example/qna/QuestionIdxRVAdapter.java
@@ -0,0 +1,63 @@
+package com.example.qna;
+
+import android.os.Build;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class QuestionIdxRVAdapter extends RecyclerView.Adapter<QuestionIdxRVAdapter.ViewHolder> {
+    ArrayList<UserData.Question_list_data> questionDataList;
+    OnItemClickListener mlistener;
+    public QuestionIdxRVAdapter(ArrayList<UserData.Question_list_data> questionDataList) {
+        this.questionDataList = questionDataList;
+    }
+    interface OnItemClickListener{
+        public void onItemClick(View view,int position);
+    }
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener){
+        mlistener = onItemClickListener;
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View v = layoutInflater.inflate(R.layout.question_list_wrapper,parent,false);
+        return new ViewHolder(v);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        holder.category.setText(String.join("/",questionDataList.get(position).questionData.category));
+        holder.date.setText(questionDataList.get(position).date);
+        holder.context.setText(questionDataList.get(position).questionData.context);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mlistener.onItemClick(holder.itemView,position);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return questionDataList.size();
+    }
+    class ViewHolder extends RecyclerView.ViewHolder{
+        TextView date = itemView.findViewById(R.id.question_wrapper_date);
+        TextView category = itemView.findViewById(R.id.question_wrapper_category);
+        TextView context = itemView.findViewById(R.id.question_wrapper_context);
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
--- app/src/main/java/com/example/qna/QuestionIndexActivity.java
+++ app/src/main/java/com/example/qna/QuestionIndexActivity.java
@@ -1,14 +1,45 @@
 package com.example.qna;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.database.DatabaseReference;
+
+import java.io.Serializable;
 
 public class QuestionIndexActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    LinearLayoutManager layoutManager;
+    QuestionIdxRVAdapter adapter;
+    DatabaseReference dataRef;
+    String uid = new String();
+    UserData userData = new UserData();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_index);
+        recyclerView = findViewById(R.id.question_index_recyclerview);
+        layoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
+        layoutManager.setStackFromEnd(true);
+        adapter = new QuestionIdxRVAdapter(userData.dailyAnswer);
+        adapter.setOnItemClickListener(new QuestionIdxRVAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+//              Intent itemIntent = new Intent(this,DailyQuestionActivity.class);
+                //itemIntent.putExtra("QuestionData", userData.dailyAnswer.get(position).questionData);
+             // startActivity(itemIntent);
+            }
+        });
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
     }
 }
